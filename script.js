5b3a2753e4add83d172e15309b718b7f0c391c5d
ChrisGenschow <91728844+ChrisGenschow@users.noreply.github.com>
2022-10-03 03:25:15 +0200

    Added character movement, and linked collisions.js to the html for further use

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const canvas = document.querySelector("canvas");
 
 const context = canvas.getContext("2d");
 
-context.fillStyle = "white";
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 //Generate different images for player and map.
 const backgroundImage = new Image();
 backgroundImage.src = "./gameTwoAssets/tilemapforbrowsergame.png";
@@ -20,7 +17,7 @@ class Sprite {
   }
 
   draw() {
-    context.drawImage(this.backgroundImage, -631, -432);
+    context.drawImage(this.backgroundImage, this.position.x, this.position.y);
   }
 }
 
@@ -43,8 +40,24 @@ playerImage.onload = () => {
   );
 };
 
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
+  background.draw();
   context.drawImage(
     playerImage,
     canvas.width / 2 - playerImage.width / 2,
@@ -52,24 +65,47 @@ function animate() {
     8,
     7
   );
-  console.log("animatePlayer");
-  background.draw();
+  if (keys.w.pressed && lastKeyPressed === "w") background.position.y += 1;
+  else if (keys.a.pressed && lastKeyPressed === "a") background.position.x += 1;
+  else if (keys.s.pressed && lastKeyPressed === "s") background.position.y -= 1;
+  else if (keys.d.pressed && lastKeyPressed === "d") background.position.x -= 1;
 }
 animate();
 
+let lastKeyPressed = "";
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
-      console.log("w meep meep");
+      keys.w.pressed = true;
+      lastKeyPressed = "w";
       break;
     case "a":
-      console.log("a meep meep");
+      keys.a.pressed = true;
+      lastKeyPressed = "a";
       break;
     case "s":
-      console.log("s meep meep");
+      lastKeyPressed = "s";
+      keys.s.pressed = true;
       break;
     case "d":
-      console.log("d meep meep");
+      lastKeyPressed = "d";
+      keys.d.pressed = true;
+      break;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
       break;
   }
 });
